2e5370530680ef9b60fa1e1ad0dfa1794882fa03
aparna <aparna9imakaa@gmail.com>
2024-02-29 18:46:48 -0500

    second commit

--- src/test/java/resources/login.feature
+++ src/test/java/resources/login.feature
@@ -6,10 +6,10 @@ Then user logs into the homepage
 Scenario Outline: Check login feature with invalid set of username and Password
 Given User enters invalid "<username>" and "<password>" 
 When user clicks on submit button
-Then user fails to log into the homepage with error message "<Error Message>"
+Then user fails to log into the homepage with error message
 Examples:
-   username		| password  | Error Message
-  standard_user	| sauce	   	|	Epic sadface: Username and password do not match any user in this service
-  standard_user	| labs		|	Epic sadface: Username and password do not match any user in this service
- 	standard	| labs		|	Epic sadface: Username and password do not match any user in this service
+ | username			| password  |
+ | standard_user	| sauce 	|	
+ |standard_user		| labs		|	 
+ |standard			| labs		|	
   
--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -15,47 +15,49 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginSteps {
 	private WebDriver driver;
-//	@Before
-//	public void setUp() {
-//		WebDriverManager.chromedriver().setup();
-//		driver =  new ChromeDriver();
-//
-//	}
-//	@After
-//	public void tearDown() {
-//		if(driver!=null) {
-//			driver.quit();
-//		}
-//	}
-//	LoginPage login = new LoginPage(driver);
-//	HomePage home = new HomePage(driver);
-//	@Given("User is on Login page")
-//	public void user_is_on_login_page(String email, String Password) {
-//		login.username("standard_user");
-//		login.password("secret_sauce");
-//
-//	}
-//
-//	@Then("User needs to Login")
-//	public void user_needs_to_login() {
-//		login.submit();		    		}
-//
-//	@Given("User enters valid username and password")
-//	public void user_enters_valid_username_and_password() {
-//		// Write code here that turns the phrase above into concrete actions
-//		login.username("standard_user");
-//		login.password("secret_sauce");
-//	}
-//
-//	@When("user clicks on submit button")
-//	public void user_clicks_on_submit_button() {
-//		login.submit();
-//	}
-//
-//	@Then("user logs into the homepage")
-//	public void user_logs_into_the_homepage() {
-//		home.showPage();
-//	}
+	@Before
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver =  new ChromeDriver();
+
+	}
+	@After
+	public void tearDown() {
+		if(driver!=null) {
+			driver.quit();
+		}
+	}
+	LoginPage login = new LoginPage(driver);
+	HomePage home = new HomePage(driver);
+	@Given("User enters valid username and password")
+	public void user_enters_valid_username_and_password() {
+	    // Write code here that turns the phrase above into concrete actions
+login.username("standard_user");
+login.password("secret_sauce");
+}
+
+	@When("user clicks on submit button")
+	public void user_clicks_on_submit_button() {
+	    // Write code here that turns the phrase above into concrete actions
+login.submit()	;}
+
+	@Then("user logs into the homepage")
+	public void user_logs_into_the_homepage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User enters invalid {string} and {string}")
+	public void user_enters_invalid_and(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user fails to log into the homepage with error message")
+	public void user_fails_to_log_into_the_homepage_with_error_message() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
 }
 
